67fe31e5dc201f5ca74e8cad8f82577489d328fa
Asif3359 <asifahammed359@gmail.com>
2023-12-13 09:19:36 +0600

    remove redirec tlink

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,7 @@ const handler = NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRETE,
-            // redirectUri: 'https://asif-ahammed-profile.vercel.app/api/auth/callback/google',
+            redirectUri: 'https://asif-ahammed-profile.vercel.app/api/auth/callback/google',
             // redirectUri: 'http://localhost:3000/api/auth/callback/google',
         })
     ],
