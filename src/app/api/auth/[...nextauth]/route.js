0199d3d6afcd68ed4cc10d42ff04a0ed90b4a091
Asif3359 <asifahammed359@gmail.com>
2023-12-13 06:23:19 +0600

    added sing in and sing out env variable

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,10 +4,25 @@ import GoogleProvider from "next-auth/providers/google"
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId:process.env.GOOGLE_ID,
-            clientSecret:process.env.GOOGLE_SECRETE,
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRETE,
         })
-    ]
+    ],
+    callbacks: {
+        async jwt(token, user) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session(session, user) {
+            session.user.id = user.id;
+            return session;
+        },
+    },
+    jwt: {
+        secret: process.env.NEXT_AUTH
+    },
 })
 
 export { handler as GET, handler as POST }
